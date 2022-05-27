08833ab2d61c62b530e755f0d408a6fe99dced57
JeanBilheux <bilheuxjm@ornl.gov>
2022-05-27 10:02:16 -0400

    log file in place and correct version retrieved. this refs #3

--- maverick/maverick.py
+++ maverick/maverick.py
@@ -6,11 +6,16 @@ import logging
 import warnings
 warnings.filterwarnings("ignore")
 
+from .utilities.get import Get
+from .utilities.config_handler import ConfigHandler
+
 from . import load_ui
 
 
 class MainWindow(QMainWindow):
 
+    session = {'top_folder': None,     # the base folder to start looking at images folder to combine
+              }
 
     def __init__(self, parent=None):
         """
@@ -22,22 +27,35 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
 
         self.ui = load_ui('mainWindow.ui', baseinstance=self)
-        # self.init_interface()
+        self.setup()
 
-        # configuration of config
-        # o_get = Get(parent=self)
-        # log_file_name = o_get.get_log_file_name()
-        # self.log_file_name = log_file_name
-        # logging.basicConfig(filename=log_file_name,
-        #                     filemode='a',
-        #                     format='[%(levelname)s] - %(asctime)s - %(message)s',
-        #                     level=logging.INFO)
-        # logging.info("*** Starting a new session ***")
-        # #logging.info(f" Version: {versioneer.get_versions()}")
-        # logging.info(f" Version: FIXME!")
+        o_get = Get(parent=self)
+        log_file_name = o_get.log_file_name()
+        version = o_get.version()
+        self.log_file_name = log_file_name
+        logging.basicConfig(filename=log_file_name,
+                            filemode='a',
+                            format='[%(levelname)s] - %(asctime)s - %(message)s',
+                            level=logging.INFO)
+        logging.info("*** Starting a new session ***")
+        logging.info(f" Version: {version}")
 
+    def setup(self):
+        o_config = ConfigHandler(parent=self)
+        o_config.load()
 
+        current_folder = None
+        if self.config['debugging']:
+            list_homepath = self.config['homepath']
+            for _path in list_homepath:
+                if os.path.exists(_path):
+                    current_folder = _path
+            if current_folder is None:
+                current_folder = os.path.expanduser('~')
+        else:
+            current_folder = os.path.expanduser('~')
 
+        self.session['top_folder'] = current_folder
 
 def main(args):
     app = QApplication(args)
---
+++ maverick/utilities/get.py
@@ -0,0 +1,37 @@
+import os
+from os.path import expanduser
+from pathlib import Path
+import configparser
+
+
+class Get:
+
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def log_file_name(self):
+        log_file_name = self.parent.config['log_file_name']
+        full_log_file_name = Get.full_home_file_name(log_file_name)
+        return full_log_file_name
+
+    def automatic_config_file_name(self):
+        config_file_name = self.parent.config['session_file_name']
+        full_config_file_name = Get.full_home_file_name(config_file_name)
+        return full_config_file_name
+
+    def version(self):
+        setup_cfg = 'setup.cfg'
+        this_folder = os.path.abspath(os.path.dirname(__file__))
+        top_path = Path(this_folder).parent.parent
+        full_path_setup_cfg = str(Path(top_path) / Path(setup_cfg))
+        config = configparser.ConfigParser()
+        config.read(full_path_setup_cfg)
+        version = config['metadata']['version']
+        return version
+
+
+    @staticmethod
+    def full_home_file_name(base_file_name):
+        home_folder = expanduser("~")
+        full_log_file_name = os.path.join(home_folder, base_file_name)
+        return full_log_file_name
